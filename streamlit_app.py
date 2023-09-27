a375a23492215e062dad95789b6f946d2b6ba97d
sophiandreasalvacion <146161997+sophiandreasalvacion@users.noreply.github.com>
2023-09-27 14:07:59 +0800

    Update streamlit_app.py
    Choose a Few Fruits to Set a Good Example

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#L ets put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+#Lets put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
